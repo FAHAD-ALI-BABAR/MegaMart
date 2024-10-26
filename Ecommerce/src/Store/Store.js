a06d50691d25059751b1fdd5a1f11384e4c0212d
Fahad Ali Babar <rajafahadali931@gmail.com>
2024-10-26 16:39:26 +0500

    comm

--- Ecommerce/src/Store/Store.js
+++ Ecommerce/src/Store/Store.js
@@ -10,11 +10,49 @@ const itemslice=createSlice({
         }
     }
 })
+const cartslice=createSlice({
+    name:"cart",
+    initialState:[],
+    reducers:{
+        addtoCart:(store,action)=>{
+            return store;
+        },
+        removefromcart:(store,action)=>{
+      return store;
+        }
+    }
+
+})
+const wishlistslice=createSlice({
+    name:"wishlist",
+    initialState:[],
+    reducers:{
+        addtowishlist:(store,action)=>{
+           return store; 
+        }
+    }
+})
+const ordersslice=createSlice({
+    name:"orders",
+    initialState:[],
+    reducers:{
+        placeorder:(store,action)=>{
+            return store;
+        }
+    }
+})
+
 const MegaMart=configureStore({
     reducer:{
         //items: will be named in useselector for accessing this
-        items:itemslice.reducer
+        items:itemslice.reducer,
+        cart:cartslice.reducer,
+        wishlist:wishlistslice.reducer,
+        orders:ordersslice.reducer
     }
 })
 export const itemsliceactions=itemslice.actions
+export const orderssliceactions=ordersslice.actions
+export const wishlistsliceactions=wishlistslice.actions
+export const cartsliceactions=cartslice.actions
 export default MegaMart
